543cb3fdb174dd3f3c11d1c1ab2be5613cf791f6
Glenn-Rhee <glennviktor5@gmail.com>
2023-12-30 18:34:42 +0700

    Menambahkan fitur menampilkan satu data user

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,7 +1,28 @@
-import { getAllUser } from "@/lib/controller/user/user";
+import { getAllUser, getOneUser } from "@/lib/controller/user/user";
 import { NextResponse, NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
+  const id = new URL(request.url).searchParams.get("id");
+  if (id) {
+    const idUser = parseInt(id);
+    if (idUser) {
+      const { statusCode, status, message, user } = await getOneUser(idUser);
+      return NextResponse.json({
+        status,
+        statusCode,
+        message,
+        data: user,
+      });
+    } else {
+      return NextResponse.json({
+        status: "failed",
+        statusCode: 400,
+        message: "Ooopss! cannot find id",
+        data: null,
+      });
+    }
+  }
+
   const users = await getAllUser();
   if (users.status === "failed") {
     return NextResponse.json({
--- src/lib/controller/user/user.ts
+++ src/lib/controller/user/user.ts
@@ -3,11 +3,39 @@ import { prisma } from "@/lib/prisma/client";
 export async function getAllUser() {
   try {
     const users = await prisma.user.findMany();
-    console.log(users);
-
     return { status: "success", data: users };
   } catch (error) {
     return { status: "failed", data: [] };
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+export async function getOneUser(id: number) {
+  try {
+    const user = await prisma.user.findFirst({ where: { id } });
+    if (!user) {
+      return {
+        statusCode: 404,
+        message: "User not found!",
+        user: null,
+        status: "failed",
+      };
+    }
+
+    return {
+      statusCode: 200,
+      message: "Succes get one user",
+      user,
+      status: "success",
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      message: "failed when get one data",
+      user: null,
+      status: "failed",
+    };
   } finally {
     await prisma.$disconnect();
   }
